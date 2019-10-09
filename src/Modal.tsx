70c99f3c9e89966d840ad85c936ccfb09740725a
spencerkingman-niche <skingman@niche.com>
2019-10-09 16:24:58 -0400

    Add comments

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -14,6 +14,7 @@ interface IState {
 }
 
 export class Modal extends Component<IProps, IState> {
+    // I couldn't figure out how to correctly type these callback refs.
     modalNode: any
     openButtonNode: any
     closeButtonNode: any
@@ -21,7 +22,9 @@ export class Modal extends Component<IProps, IState> {
     state = { isOpen: false }
 
     onClickAway = (e: MouseEvent) => {
-        if (this.modalNode && this.modalNode.contains(e.target)) return
+        if (this.modalNode && this.modalNode.contains(e.target)) {
+            return
+        }
         this.onClose()
     }
 
@@ -33,11 +36,14 @@ export class Modal extends Component<IProps, IState> {
 
     onOpen = () => {
         this.setState({ isOpen: true }, () => {
+            // We have to focus the closeButton in a callback to make sure it 
+            // actually exists when we call focus()
             this.closeButtonNode.focus()
         })
         this.toggleScrollLock()
     }
 
+    // 27 is the Esc key
     onKeyDown = ({ keyCode }: KeyboardEvent) => keyCode === 27 && this.onClose()
 
     toggleScrollLock = () =>
@@ -48,6 +54,8 @@ export class Modal extends Component<IProps, IState> {
     render() {
         const { isOpen } = this.state
         const { ariaLabel, children, role, triggerText } = this.props
+        // Using react.Fragment here prevents the use of a meaningless div wrapper.
+        // Fragment will allow react to render, but won't add div wrapper noise to the HTML.
         return (
             <Fragment>
                 <ModalTrigger
@@ -55,7 +63,8 @@ export class Modal extends Component<IProps, IState> {
                     onOpen={this.onOpen}
                     text={triggerText}
                 />
-                {isOpen && (
+                {// When the modal is not opened it is not rendered into the DOM
+                    isOpen && (
                     <ModalContent
                         ariaLabel={ariaLabel}
                         buttonRef={(n: any) => (this.closeButtonNode = n)}
